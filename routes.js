0c57e798877c11007f82faabefd3d666405a59e3
emibtsu <emilytsujikawa@gmail.com>
2022-10-11 13:44:28 -0700

    Add some dependencies and add some basic query calls

--- routes.js
+++ routes.js
@@ -1,21 +1,21 @@
+// run as node routes.js <PW>
 var express = require('express');
 var app  = express();
 
 var mysql = require('mysql');
 var bodyParser = require('body-parser');
 var pw =  process.argv[2];
+var str="";
 app.use(bodyParser.json({type:'application/json'}));
 app.use(bodyParser.urlencoded({extended:true}));
 
-
-  console.error("Password is:"+pw);
 
 var con = mysql.createConnection({
  
     host:'localhost',
     port:'3306',
     user:'root',
-    password: pw, //need to figure out how to auto connect...
+    password: pw, //pw enter as 'node routes.js <pw>'
     database: 'mindful_db'
 
 });
@@ -32,7 +32,65 @@ con.connect(function(error){
   else console.log("connected");
   con.query("SELECT * FROM USER", function (err, result, fields) {
     //if (err) throw err;
+    console.log("USERS ");
+    str=JSON.stringify(result);
     console.log(result);
   });
+  con.query("SELECT * FROM POST", function (err, result, fields) {
+    //if (err) throw err;
+    console.log("POSTS ");
+    str=JSON.stringify(result);
+    console.log(result);
+  });
+  con.query("SELECT COUNT(*) FROM USER", function (err, result, fields) {
+    //if (err) throw err;
+    console.log("Users count ");
+    str=JSON.stringify(result);
+    if(str!=null){
+      str=str.charAt(str.length-3);
+      } else {
+        str="0";
+      }
+    console.log(str);
+  });
+  con.query("SELECT COUNT(*) FROM POST", function (err, result, fields) {
+    //if (err) throw err;
+    console.log("Posts count ");
+    str=JSON.stringify(result);
+    if(str!=null){
+      str=str.charAt(str.length-3);
+      } else {
+        str="0";
+      }
+    console.log(str);
+  });
+  con.query("SELECT * FROM FRIEND", function (err, result, fields) {
+    //if (err) throw err;
+    console.log("FRIEND CONNECTIONS ");
+    console.log(result);
+  });
+});
+con.query("SELECT COUNT(*) FROM FRIEND", function (err, result, fields) {
+  //if (err) throw err;
+  console.log("Friend connection count ");
+  str=JSON.stringify(result);
+  if(str!=null){
+    str=str.charAt(str.length-3);
+    } else {
+      str="0";
+    }
+  console.log(str);
+});
+var userSearch = "Emibtsu";
+con.query("SELECT COUNT(*) FROM POST WHERE Pusername = " + userSearch, function (err, result, fields) {
+  //if (err) throw err;
+  console.log(userSearch+ " friend count ");
+  str=JSON.stringify(result);
+  if(str!=null){
+  str=str.charAt(str.length-3);
+  } else {
+    str="0";
+  }
+    console.log(str);
 });
 
